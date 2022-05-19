0f42416d52aef7ea45ab28d80ed2db8d75a994f9
Milton Vandol Goh <miltonvandol@gmail.com>
2022-05-18 20:04:55 -0700

    Created & style the form using CSS, & implemented the char count for words using Jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('textarea').on("input", function() {
+    const counter = $(this).next().children(".counter");
+    const length = $(this).val().length;
+    const maxWord = '140';
+    const count = maxWord - length;
+    $(counter).val(count);
+
+    if (count < 0) {
+      $('#counter').css('color', 'red');
+    } else {
+      $('#counter').css('color', 'black');
+    }
+  });
+});
