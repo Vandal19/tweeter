47ba5e61e1d67546bd4246184e64522935f8195e
Milton Vandol Goh <miltonvandol@gmail.com>
2022-05-21 18:20:54 -0700

    create a => loadTweets to fetch from /tweets using jQuery & displaying time passed for each tweet using timeago library

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1652826970190
-    }
-  ];
 
-  const createTweetElement = function (data) {
+  const createTweetElement = function(data) {
     const { name, avatars, handle } = data.user;
     const content = data.content.text;
     const created = data.created_at;
@@ -47,6 +23,7 @@ $(document).ready(() => {
           <p>${content}</p>
         </div>
         <footer>
+          <span>${timeago.format(data.created_at)}</span>
           <div>
             <p>${created}</p>
           </div>
@@ -68,8 +45,6 @@ $(document).ready(() => {
     }
   };
 
-  renderTweets(data);
-
 
   $("form").submit(function (event) {
     event.preventDefault();
@@ -82,4 +57,15 @@ $(document).ready(() => {
     });
   });
 
+  const loadTweets = function() {
+    $.ajax({
+      type: "Get",
+      url: "/tweets",
+      data: "json",
+    })
+      .then(function (data) {
+        renderTweets(data);
+      });
+  };
+  loadTweets();
 });
